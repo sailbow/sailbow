4ecb795c09b951f477dce628e42b789d12b35e3e
Jacob Guth <jguth@perkspot.com>
2021-10-04 19:48:08 -0500

    add test endpoint

---
+++ sb-backend/Sb.Api/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sb.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet("/helloworld")]
+        public object HelloWorldTest()
+        {
+            return new
+            {
+                Message = "Hello world!"
+            };
+        }
+    }
+}
