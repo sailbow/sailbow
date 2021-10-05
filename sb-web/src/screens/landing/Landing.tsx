74765cd3a472f76ca2155833b5f1c1905d54666b
Hrisikesh Paul <hrishikeshpaul12@gmail.com>
2021-10-04 22:36:07 -0500

    chore: added image

--- sb-web/src/screens/landing/Landing.tsx
+++ sb-web/src/screens/landing/Landing.tsx
@@ -9,50 +9,63 @@ import { ReactComponent as LogoType } from 'assets/sb-gradient-logo-type.svg';
 import 'screens/landing/Landing.scss';
 
 export const Landing: FunctionComponent = () => {
-    return (
-        <Box className="sb-landing">
-            <Flex justifyContent="center" alignItems="center" h="100%">
-                <Box boxShadow="xl" bg="white" p="12" pb="4" borderRadius="xl">
-                    <VStack spacing="32">
-                        <Flex textAlign="center" justifyContent="center" flexDir="column">
-                            <LogoType />
-                            <Text fontSize="10px" pt="2" fontWeight="semibold" color="gray.500">
-                                Get The Crew. Plan The Voyage. <br />
-                                Set Sail.
+    const LandingCard: FunctionComponent = () => {
+        return (
+            <Box boxShadow="xl" bg="white" p="12" pb="4" borderRadius="xl">
+                <VStack spacing="32">
+                    <Flex textAlign="center" justifyContent="center" flexDir="column">
+                        <LogoType />
+                        <Text fontSize="10px" pt="2" fontWeight="semibold" color="gray.500">
+                            Get The Crew. Plan The Voyage. <br />
+                            Set Sail.
+                        </Text>
+                    </Flex>
+                    <VStack spacing="6">
+                        <Box textAlign="center">
+                            <Heading fontSize="md">Start Sailing!</Heading>
+                            <Center>
+                                <Divider pt="1" width="20%" />
+                            </Center>
+                        </Box>
+                        <VStack spacing="4">
+                            <Button
+                                variant="outline"
+                                colorScheme="gray"
+                                leftIcon={<GoogleIcon />}
+                                rightIcon={<RightIcon />}
+                            >
+                                <Text pr="8">Continue with Google</Text>
+                            </Button>
+                            <Button
+                                variant="outline"
+                                colorScheme="gray"
+                                leftIcon={<FacebookIcon />}
+                                rightIcon={<RightIcon />}
+                            >
+                                <Text pr="3.5">Continue with Facebook</Text>
+                            </Button>
+                            <Text fontSize="8px" pt="4">
+                                Copyright © 2021 Sailboat
                             </Text>
-                        </Flex>
-                        <VStack spacing="6">
-                            <Box textAlign="center">
-                                <Heading fontSize="md">Start Sailing!</Heading>
-                                <Center>
-                                    <Divider pt="1" width="20%" />
-                                </Center>
-                            </Box>
-                            <VStack spacing="4">
-                                <Button
-                                    variant="outline"
-                                    colorScheme="gray"
-                                    leftIcon={<GoogleIcon />}
-                                    rightIcon={<RightIcon />}
-                                >
-                                    <Text pr="8">Continue with Google</Text>
-                                </Button>
-                                <Button
-                                    variant="outline"
-                                    colorScheme="gray"
-                                    leftIcon={<FacebookIcon />}
-                                    rightIcon={<RightIcon />}
-                                >
-                                    <Text pr="3.5">Continue with Facebook</Text>
-                                </Button>
-                                <Text fontSize="8px" pt="4">
-                                    Copyright © 2021 Sailboat
-                                </Text>
-                            </VStack>
                         </VStack>
                     </VStack>
-                </Box>
+                </VStack>
+            </Box>
+        );
+    };
+
+    return (
+        <Flex className="sb-landing">
+            <Flex w="65%" className="left-container" h="100%" display={{ base: 'none', lg: 'block' }} />
+            <Flex
+                w={{ base: '100%', lg: '45%' }}
+                className="right-container"
+                bgColor="white"
+                justifyContent="center"
+                alignItems="center"
+            >
+                <LandingCard />
             </Flex>
-        </Box>
+        </Flex>
     );
 };
