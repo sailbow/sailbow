023fa0b20aeedc3cb50f004ae827cb2c3b6ccd9b
Jacob Guth <jguth@perkspot.com>
2021-10-12 18:43:23 -0500

    fix nested class

--- sb-backend/Sb.Api/Controllers/OAuthToken.cs
+++ sb-backend/Sb.Api/Controllers/OAuthToken.cs
@@ -5,18 +5,15 @@ using System;
 
 namespace Sb.Api.Controllers
 {
-    public partial class AuthController
+    public class OAuthToken
     {
-        public class OAuthToken
-        {
-            [JsonProperty("access_token")]
-            public string AccessToken { get; set; }
-            [JsonProperty("refresh_token")]
-            public string RefreshToken { get; set; }
-            [JsonProperty("issued_utc")]
-            public DateTimeOffset? IssuedUtc { get; set; }
-            [JsonProperty("expires_utc")]
-            public DateTimeOffset? ExpiresUtc { get; set; }
-        }
+        [JsonProperty("access_token")]
+        public string AccessToken { get; set; }
+        [JsonProperty("refresh_token")]
+        public string RefreshToken { get; set; }
+        [JsonProperty("issued_utc")]
+        public DateTimeOffset? IssuedUtc { get; set; }
+        [JsonProperty("expires_utc")]
+        public DateTimeOffset? ExpiresUtc { get; set; }
     }
 }
