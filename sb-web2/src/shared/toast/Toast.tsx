5af391887580f71e08d9b42a234814781075c767
Hrishikesh <hrishikeshpaul12@gmail.com>
2022-09-08 13:53:15 -0500

    chore: added axios and toast

---
+++ sb-web2/src/shared/toast/Toast.tsx
@@ -0,0 +1,34 @@
+import { createStandaloneToast } from '@chakra-ui/react';
+
+import { theme } from 'theme';
+
+const { toast } = createStandaloneToast({ theme });
+
+enum ToastId {
+    Error,
+    Success,
+}
+
+export const showErrorToast = (message: string) => {
+    toast.close(ToastId.Error);
+
+    toast({
+        id: ToastId.Error,
+        title: 'Oops! An error occurred',
+        description: message,
+        status: 'error',
+        position: 'top-right',
+    });
+};
+
+export const showSuccessToast = (message: string, title?: string) => {
+    toast.close(ToastId.Success);
+
+    toast({
+        id: ToastId.Success,
+        title: title || 'Success!',
+        description: message,
+        status: 'success',
+        position: 'top-right',
+    });
+};
