f120705f89d2a0e7406ff3171b1422b686d6d9a5
Jacob Guth <jguth@perkspot.com>
2022-10-26 09:35:23 -0500

    initial module classes

---
+++ sb-backend/lib/Sb.Data/Models/Module.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sb.Data.Models
+{
+    [PersistenceModel("Modules")]
+    public class Module : EntityBase
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Order { get; set; }
+        public int? FinalizedOptionId { get; set; }
+        public ModuleSettings Settings { get; set; }
+        public List<ModuleData> Data { get; set; } = new List<ModuleData>();
+
+    }
+    public class ModuleSettings
+    {
+        public bool AllowMultiple { get; set; }
+        public bool AnonymousVoting { get; set; }
+        public DateTime? Deadline { get; set; }
+    }
+    public class ModuleData : EntityBase
+    {
+        public List<string> Votes { get; set; } = new List<string>();
+        public string Author { get; set; }
+    }
+}
