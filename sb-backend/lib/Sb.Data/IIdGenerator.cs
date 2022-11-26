a70c96920676958ac5771c1bf0335803eb14643b
Jacob Guth <jacobguth96@gmail.com>
2022-11-26 13:08:30 -0600

    run code cleanup

--- sb-backend/lib/Sb.Data/IIdGenerator.cs
+++ sb-backend/lib/Sb.Data/IIdGenerator.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Sb.Data
+﻿namespace Sb.Data
 {
     public interface IIdGenerator
     {
