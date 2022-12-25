6862720ff3f44830fcfd16fab77a9ebcef7054ed
jrguth <jacobguth96@gmail.com>
2022-12-25 16:22:49 -0600

    fix spacing

--- sb-backend/Sb.Api/Controllers/ModuleOptionsController.cs
+++ sb-backend/Sb.Api/Controllers/ModuleOptionsController.cs
@@ -10,25 +10,30 @@ namespace Sb.Api.Controllers
     [Route("api/boats/{boatId}/modules/{moduleId}/options")]
     public class ModuleOptionsController : ApiControllerBase
     {
-		public ModuleOptionsController
-			(IModuleService moduleService)
-		{
-			_moduleService = moduleService;
-		}
+        public ModuleOptionsController(
+            IModuleService moduleService)
+        {
+            _moduleService = moduleService;
 
-		[HttpPost]
-		public async Task<ModuleData> AddModuleOption([FromBody] ModuleData moduleData)
-		{
-			return await _moduleService.AddModuleData(moduleData);
-		}
+        }
 
-		[HttpDelete("{optionId}")]
-		public async Task DeleteModuleOption(string optionId)
-		{
-			await _moduleService.DeleteModuleData(optionId);
-		}
+        [HttpPost]
+        public async Task<ModuleData> AddModuleOption(
+        [FromRoute] string boatId,
+        [FromRoute] string moduleId,
+        [FromBody] ModuleData moduleData)
+        {
+            moduleData.ModuleId = moduleId;
+            moduleData.Author = HttpContext.GetUserId();
+            return await _moduleService.AddModuleData(moduleData);
+        }
 
-		private readonly IModuleService _moduleService;
-	}
+        [HttpDelete("{optionId}")]
+        public async Task DeleteModuleOption(string optionId)
+        {
+            await _moduleService.DeleteModuleData(optionId);
+        }
+
+        private readonly IModuleService _moduleService;
+    }
 }
-
