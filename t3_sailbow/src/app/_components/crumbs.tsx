850dc80a17d37b3f9d21d27a856727c8462c9107
jrguth <jacobguth96@gmail.com>
2024-05-01 18:37:45 -0500

    fix

--- t3_sailbow/src/app/_components/crumbs.tsx
+++ t3_sailbow/src/app/_components/crumbs.tsx
@@ -6,7 +6,6 @@ import {
   BreadcrumbItem,
   BreadcrumbLink,
   BreadcrumbSeparator,
-  BreadcrumbPage,
 } from "@/components/ui/breadcrumb";
 import { Button } from "@/components/ui/button";
 import {
