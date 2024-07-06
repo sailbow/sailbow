a33efff79871df75f1d11ba9add9708e20e83e6d
jrguth <jacobguth96@gmail.com>
2024-07-06 11:58:08 -0500

    improved settings page; delete boat modal

---
+++ t3_sailbow/src/app/dock/[boatId]/settings/delete-boat.tsx
@@ -0,0 +1,86 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { api } from "@/trpc/react";
+import { useRouter } from "next/navigation";
+import { toast } from "@/components/ui/toast";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { DialogDescription } from "@radix-ui/react-dialog";
+
+export default function DeleteBoat({ boatId }: { boatId: number }) {
+  const router = useRouter();
+  const { mutateAsync: deleteBoatById, isLoading: isDeletingBoat } =
+    api.dock.deleteBoatById.useMutation({
+      onError: (error) => {
+        toast.dismiss();
+        if (error.data?.code === "UNAUTHORIZED") {
+          toast.error("You do not have permission to delete this boat!");
+        } else {
+          toast.error("Something went wrong, please try again later");
+        }
+      },
+    });
+
+  const deleteBoat = async () => {
+    toast.promise(deleteBoatById({ boatId: boatId }), {
+      loading: "Deleting...",
+      success: () => {
+        router.push("/dock");
+        router.refresh();
+        return "Success!";
+      },
+    });
+  };
+
+  return (
+    <Card className="w-full">
+      <CardHeader>
+        <CardTitle>Danger Zone</CardTitle>
+        <CardDescription>These actions cannot be undone</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant="destructive">Delete boat</Button>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>
+                Are you sure you want to delete this boat?
+              </DialogTitle>
+              <DialogDescription>
+                This action cannot be undone!
+              </DialogDescription>
+            </DialogHeader>
+            <DialogFooter>
+              <DialogClose asChild>
+                <Button variant="secondary">Cancel</Button>
+              </DialogClose>
+              <Button
+                variant="destructive"
+                onClick={deleteBoat}
+                disabled={isDeletingBoat}
+              >
+                Delete
+              </Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
+      </CardContent>
+    </Card>
+  );
+}
--- t3_sailbow/src/app/dock/[boatId]/settings/page.tsx
+++ t3_sailbow/src/app/dock/[boatId]/settings/page.tsx
@@ -1,43 +1,15 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { toast } from "@/components/ui/toast";
-import { api } from "@/trpc/react";
-import { useRouter } from "next/navigation";
+import DeleteBoat from "./delete-boat";
 
 export default function Page({ params }: { params: { boatId: string } }) {
-  const router = useRouter();
-  const { mutate: deleteBoatById, isLoading: isDeletingBoat } =
-    api.dock.deleteBoatById.useMutation({
-      onSuccess: async () => {
-        router.push("/dock");
-        router.refresh();
-      },
-      onError: (error) => {
-        if (error.data?.code === "UNAUTHORIZED") {
-          toast.error("You do not have permission to delete this boat!");
-        }
-      },
-    });
-
-  const deleteBoat = () => {
-    deleteBoatById({ boatId: parseInt(params.boatId) });
-  };
-
   return (
     <div className="size-full overflow-y-auto">
       <div className="space-y-2">
         <h1 className="text-xl leading-none tracking-tight sm:text-3xl">
           Settings
         </h1>
-        <Button
-          size="sm"
-          variant="destructive"
-          disabled={isDeletingBoat}
-          onClick={deleteBoat}
-        >
-          Delete boat
-        </Button>
+        <DeleteBoat boatId={parseInt(params.boatId)} />
       </div>
     </div>
   );
