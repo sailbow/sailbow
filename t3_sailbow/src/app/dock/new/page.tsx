ef58d84dff43bf685b6ed398d9833fc2597d1977
jrguth <jacobguth96@gmail.com>
2024-07-11 19:46:48 -0500

    try to prevent zoom on new boat page for mobile

--- t3_sailbow/src/app/dock/new/page.tsx
+++ t3_sailbow/src/app/dock/new/page.tsx
@@ -1,5 +1,14 @@
 import { CreateBoatForm } from "@/app/_components/create-boat-form";
+import { type Viewport } from "next/types";
 
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+  // Also supported by less commonly used
+  // interactiveWidget: 'resizes-visual',
+};
 export default function NewBoatPage() {
   return (
     <div className="container mx-auto h-full max-w-2xl py-4">
