5ad7ded7ef19af84988ce1646995b2c77e29e85e
jrguth <jacobguth96@gmail.com>
2024-07-11 22:09:34 -0500

    move viewport meta to root layout; change input text size

--- t3_sailbow/src/app/dock/new/page.tsx
+++ t3_sailbow/src/app/dock/new/page.tsx
@@ -1,14 +1,5 @@
 import { CreateBoatForm } from "@/app/_components/create-boat-form";
-import { type Viewport } from "next/types";
 
-export const viewport: Viewport = {
-  width: "device-width",
-  initialScale: 1,
-  maximumScale: 1,
-  userScalable: false,
-  // Also supported by less commonly used
-  // interactiveWidget: 'resizes-visual',
-};
 export default function NewBoatPage() {
   return (
     <div className="container mx-auto h-full max-w-2xl py-4">
