236b4066be3b2d96a31231eb5ae6e3f9ccc9133c
jrguth <jacobguth96@gmail.com>
2024-09-05 17:22:43 -0500

    Update actions.ts

--- sb-web/convex/invitations/actions.ts
+++ sb-web/convex/invitations/actions.ts
@@ -3,7 +3,7 @@ import { v } from "convex/values";
 import { clerkClient } from "@convex/_lib/clerk";
 
 function getBaseUrl() {
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
+  if (process.env.NEXT_PUBLIC_VERCEL_URL) return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
   return `http://localhost:${process.env.PORT ?? 3000}`;
 }
 
