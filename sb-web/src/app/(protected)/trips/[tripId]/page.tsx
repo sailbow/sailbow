d034302f23bee29ba7f638fce5191e9321a95c97
jrguth <jacobguth96@gmail.com>
2024-09-15 11:13:30 -0500

    temp push

--- sb-web/src/app/(protected)/trips/[tripId]/page.tsx
+++ sb-web/src/app/(protected)/trips/[tripId]/page.tsx
@@ -9,7 +9,13 @@ import {
 import HomePageContent from "./home-page-content";
 import TripBannerModal from "./trip-banner-modal";
 import { useMe } from "@/lib/user-queries";
-import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 export default function TripOverviewPage() {
   const { data: trip } = useTrip();
@@ -18,7 +24,8 @@ export default function TripOverviewPage() {
   return (
     <BoatPageContainer>
       <BoatPageHeader>
-        <BoatPageTitle>Welcome, {me.firstName}!</BoatPageTitle>
+        {/* <BoatPageTitle>Welcome, {me.firstName}!</BoatPageTitle> */}
+        <BoatPageTitle>{trip.name}</BoatPageTitle>
         {!trip.banner && (
           <div className="ml-auto">
             <TripBannerModal />
@@ -26,18 +33,23 @@ export default function TripOverviewPage() {
         )}
       </BoatPageHeader>
       <BoatPageContent>
-        <div className="grid flex-1 grid-cols-2 gap-4">
+        {/* <div className="grid w-full grid-cols-1 gap-4 sm:grid-cols-2">
           <div className="flex items-center gap-4">
             <Card>
+              <CardHeader className="border-b p-2">
+                <CardTitle className="text-sm font-light">Trip name</CardTitle>
+              </CardHeader>
+              <CardContent className="p-2">
+                <p className="text-base font-semibold">{trip.name}</p>
+              </CardContent>
+            </Card>
+            <Card>
               <CardHeader>
-                {/* <div className="text-xs font-light">Trip name</div> */}
-                <CardTitle className="text-center font-normal">
-                  Trip {trip.name}
-                </CardTitle>
+                <CardTitle className="font-normal">Crew members</CardTitle>
               </CardHeader>
             </Card>
           </div>
-        </div>
+        </div> */}
         <HomePageContent />
       </BoatPageContent>
     </BoatPageContainer>
