ded5eec8a366e601c640467f82c52de1319fc287
jrguth <jacobguth96@gmail.com>
2025-07-07 10:41:46 -0500

    Update poll-results-chart.tsx

--- sb-web/src/components/poll-results-chart.tsx
+++ sb-web/src/components/poll-results-chart.tsx
@@ -1,6 +1,4 @@
 "use client";
-
-import { TrendingUp } from "lucide-react";
 import {
   Bar,
   BarChart,
@@ -11,65 +9,14 @@ import {
 } from "recharts";
 
 import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import {
-  ChartConfig,
   ChartContainer,
   ChartTooltip,
   ChartTooltipContent,
 } from "@/components/ui/chart";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-} from "./ui/dialog";
 import { Poll } from "./types";
 import { Id } from "@convex/_generated/dataModel";
-import { o } from "node_modules/framer-motion/dist/types.d-B_QPEvFK";
 
-export const description = "A bar chart with a custom label";
-
-const chartData = [
-  { month: "January", desktop: 186, mobile: 80 },
-  { month: "February", desktop: 305, mobile: 200 },
-  { month: "March", desktop: 237, mobile: 120 },
-  { month: "April", desktop: 73, mobile: 190 },
-  { month: "May", desktop: 209, mobile: 130 },
-  { month: "June", desktop: 214, mobile: 140 },
-];
-
-const chartConfig = {
-  desktop: {
-    label: "Desktop",
-    color: "var(--chart-2)",
-  },
-  mobile: {
-    label: "Mobile",
-    color: "var(--chart-2)",
-  },
-  label: {
-    color: "var(--background)",
-  },
-} satisfies ChartConfig;
-
-export function PollResultsChart({
-  // open,
-  // onOpenChange,
-  poll,
-}: {
-  // open: boolean;
-  // onOpenChange: (open: boolean) => void;
-  poll: Poll;
-}) {
-  console.log(poll);
+export function PollResultsChart({ poll }: { poll: Poll }) {
   const responsesByChoice = new Map<
     Id<"pollOptions">,
     { id: Id<"pollOptions">; label: string; count: number }
